02845a948510cc7ad4dba2b2bce236568cd1319b
yanntyb <69966391+yanntyb@users.noreply.github.com>
2020-12-04 10:24:38 +0100

    a

--- exercice1.js
+++ exercice1.js
@@ -1,10 +1,18 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+
+monDiv.innerHTML = "div ajouté en js";
+
+let monDiv2 = document.createElement("div");
+monDiv2.style.backgroundColor = "red";
+monDiv2.style.width = "100px";
+monDiv2.style.height = "100px";
+
 
 document.getElementById('viewport').appendChild(monDiv);
+document.getElementById("viewport").appendChild(monDiv2);
 
 
-
-
+document.getElementById("aSupprimer").remove()
 
 
 
